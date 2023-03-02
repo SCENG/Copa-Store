8bc64095f7009d13dd4efe78f50bebab005b7220
Elijah Sinkamba <Chisanga110@live.com>
2023-03-02 21:19:35 +0200

    added more products

--- src/products.js
+++ src/products.js
@@ -7,36 +7,92 @@ import product6 from "./assets/products/6.webp";
 import product7 from "./assets/products/7.webp";
 import product8 from "./assets/products/8.webp";
 
+
+
+import watch_1 from './assets/products/watch_1.jpg'
+import watch_2 from './assets/products/watch_2.jpg'
+import watch_3 from './assets/products/watch_3.jpg'
+
+import sports_1 from './assets/products/sports_1.jpg'
+import sports_2 from './assets/products/sports_2.jpg'
+import sports_3 from './assets/products/sports_3.jpg'
+import sports_4 from './assets/products/sports_4.jpg'
+import sports_6 from './assets/products/sports_6.jpg'
+
+import shoe_1 from './assets/products/shoe_1.jpg'
+import shoe_2 from './assets/products/shoe_2.jpg'
+import shoe_3 from './assets/products/shoe_3.jpg'
+import shoe_4 from './assets/products/shoe_4.jpg'
+
+
+
+import jacket_1 from './assets/products/jacket_1.jpg'
+import jacket_2 from './assets/products/jacket_2.jpg'
+import jacket_3 from './assets/products/jacket_3.jpg'
+import jacket_4 from './assets/products/jacket_4.jpg'
+import jacket_5 from './assets/products/jacket_5.jpg'
+import jacket_6 from './assets/products/jacket_6.jpg'
+
+import shorts_1 from './assets/products/shorts_1.jpg'
+
+import jewellery_1 from './assets/products/jewellery_1.jpg'
+import jewellery_2 from './assets/products/jewellery_2.jpg'
+import jewellery_3 from './assets/products/jewellery_3.jpg'
+
+import clothes_1 from './assets/products/clothes_1.jpg'
+import clothes_2 from './assets/products/clothes_2.jpg'
+import clothes_3 from './assets/products/clothes_3.jpg'
+import clothes_4 from './assets/products/clothes_4.jpg'
+
+import one from './assets/products/one.jpg'
+import two from './assets/products/two.jpg'
+import three from './assets/products/three.jpg'
+import four from './assets/products/four.jpg'
+
+import belt from './assets/products/belt.jpg'
+
+import party_wear_1 from './assets/products/party_wear_1.jpg'
+import party_wear_2 from './assets/products/party_wear_2.jpg'
+
+import perfume from './assets/products/perfume.jpg'
+import shampoo from './assets/products/shampoo.jpg'
+
+import shirt_1 from './assets/products/shirt_1.jpg'
+import shirt_2 from './assets/products/shirt_2.jpg'
+
+
+
+
 export const PRODUCTS = [
   {
     id: 1,
-    productName: "IPhone",
-    price: 999.0,
-    productImage: product1,
+    productName: "BELT",
+    price: 99.0,
+    productImage: belt,
   },
   {
     id: 2,
-    productName: "Macbook Pro 2022 (M1)",
-    price: 1999.0,
-    productImage: product2,
+    productName: "PINK JUMPER",
+    price: 59.0,
+    productImage: clothes_1,
   },
   {
     id: 3,
-    productName: "Cannon M50 Camera",
-    price: 699.0,
-    productImage: product3,
+    productName: "NECKLACE",
+    price: 15.0,
+    productImage: jewellery_3,
   },
   {
     id: 4,
-    productName: "WLS Van Gogh Denim Jacket",
-    price: 228.0,
-    productImage: product4,
+    productName: "LIGHT BROWN JACKET",
+    price: 120.0,
+    productImage: jacket_1,
   },
   {
     id: 5,
-    productName: "LED Light Strips",
-    price: 19.99,
-    productImage: product5,
+    productName: "DARK BROWN JACKET",
+    price: 120.0,
+    productImage: jacket_2,
   },
   {
     id: 6,
@@ -46,14 +102,58 @@ export const PRODUCTS = [
   },
   {
     id: 7,
-    productName: "AUTO SERVICE SHIRT by GOLF WANG",
+    productName: "HEELS",
     price: 120.0,
-    productImage: product7,
+    productImage: party_wear_1,
   },
   {
     id: 8,
-    productName: "DON'T TRIP UNSTRUCTURED HAT",
+    productName: "SMART SHOE",
     price: 40.0,
-    productImage: product8,
+    productImage: shoe_1,
   },
+  {
+    id: 9,
+    productName: "DIAMOND EARRINGS",
+    price: 500.0,
+    productImage: jewellery_1,
+  },
+  {
+    id: 10,
+    productName: "PERFUME",
+    price: 60.0,
+    productImage: perfume,
+  },
+  {
+    id: 9,
+    productName: "NIKE",
+    price: 240.0,
+    productImage: sports_6,
+  },
+  {
+    id: 9,
+    productName: "SHIRT",
+    price: 40.0,
+    productImage: shirt_1,
+  },
+  {
+    id: 10,
+    productName: "SHAMPOO",
+    price: 40.0,
+    productImage: shampoo,
+  },
+  {
+    id: 9,
+    productName: "APPLE WATCH",
+    price: 700.0,
+    productImage: watch_1,
+  },
+  {
+    id: 9,
+    productName: "RUNNERS",
+    price: 230.0,
+    productImage: sports_1,
+  },
+
+  
 ];
